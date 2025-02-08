191763563b1b2efd2cec9209f820fc1fed476250
kongchatsupasri <36078290+kongchatsupasri@users.noreply.github.com>
2025-02-08 17:54:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,14 @@ import json
 
 # Detect environment (Local or Streamlit Cloud)
 if "GOOGLE_CLIENT_ID" in st.secrets:
+    st.write('aaa')
     st.write(st.secrets)
     CLIENT_ID = st.secrets["GOOGLE_CLIENT_ID"]
     CLIENT_SECRET = st.secrets["GOOGLE_CLIENT_SECRET"]
     REDIRECT_URI = "https://vat-cal.streamlit.app/"
 else:
+    st.write('bbb')
+    st.write(st.secrets)
     CLIENT_ID = "your-local-client-id.apps.googleusercontent.com"
     CLIENT_SECRET = "your-local-client-secret"
     REDIRECT_URI = "http://localhost:8501/"
